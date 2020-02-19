1fac2732a79d9fb401db34139e1bc52dd52505a0
Bradley Reeves <bradleyaaronreeves@gmail.com>
2020-02-18 18:54:20 -0800

    adding caucus dates to plot

--- src/dem_primary.R
+++ src/dem_primary.R
@@ -12,16 +12,20 @@ dem_df<-subset(dem_df, start_date > "2019-06-15")
 
 dem_df<-setNames(aggregate(dem_df$pct, by=list(dem_df$candidate_name, dem_df$start_date), FUN=mean), c("candidate_name", "start_date", "pct"))
 
+cau_df<-data.frame(date=as.Date(c("2020-02-03","2020-02-11")), events=c("IA", "NH"))
+
 colnames(dem_df)[which(names(dem_df)=="candidate_name")]<-"Candidate"
 
 ggplot(dem_df, aes(y=pct, x=start_date, group=Candidate, color=Candidate)) + 
   geom_line(size=2) + 
+  geom_vline(data=cau_df, aes(xintercept=date), color="black", linetype="longdash") +
+  annotate("label", label=d$events, x=d$date, y=48, fill="yellow") +
   labs(title="Democratic Primary Polling", y="Points", x="Date", caption="Data: www.fivethirtyeight.com") +
   scale_color_manual(values=c("#4C4CFF", "#e6194B", "#ffa500", "#00ff80", "#000000", "#ff00bf", "#008000", "#00ffff")) + 
   scale_x_date(labels=date_format("%B %y"), date_breaks="1 month") +
   theme(axis.text.x=element_text(angle=45, hjust=1))
 
-ggsave("dem_polls.pdf", units="in", width=10, height=10, dpi=300)
+ggsave("../out/dem_polls.pdf", units="in", width=10, height=10, dpi=300)
 
 #--- delegates ---#
 del_df<-read.csv("../dat/delegates.csv", header=TRUE)
@@ -35,4 +39,5 @@ ggplot(del_df, aes(x=candidate, y=delegates)) +
   geom_text(aes(label=delegates), vjust=2) +
   labs(title="Total Delegates Awarded", y="Delegates", x="Candidate", caption="Candidates need 1,991 delegates to secure their nomination on the first ballot at the Democratic National Convention")
 
+ggsave("../out/dem_dels.pdf", units="in", width=10, height=10, dpi=300)
 
